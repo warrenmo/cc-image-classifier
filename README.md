4a553ed9904ae4e74f0962d80d72b046c9ac069d
warren <wmowkm@gmail.com>
2020-12-26 00:58:16 -0500

    organizing pages, proper credit given in README

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # cc-image-classifier
-A web-app for scraping images (under the creative commons license) and classifying them quickly using deep (transfer) learning
+A web-app for scraping images (under the creative commons license) and classifying them quickly using deep (transfer) learning. Various code snippets inspired by [streamlit's self-driving demo](https://github.com/streamlit/demo-self-driving).
--- cc_image_classifier.py
+++ cc_image_classifier.py
@@ -12,20 +12,93 @@ from fastai.vision import download_images
 def main():
     instructions = st.markdown('# Test')
 
-    classes, num_images_per_class = ui()
+    st.sidebar.title("What to do next?")
+    app_mode = st.sidebar.selectbox("",
+        ["Show instructions", "Download images",
+         "Train the classifier", "Use/download the classifier",
+         "Show the source code"])
+    if app_mode == 'Show instructions':
+        st.sidebar.success('To continue select "Download images".')
+    elif app_mode == 'Download images':
+        instructions.empty()
+        download_images()
+    elif app_mode == 'Train the classifier':
+        train_classifier()
+    elif app_mode == 'Use/download the classifier':
+        run_classifier()
+    elif app_mode == 'Show the source code':
+        instructions.empty()
+        st.code(get_file_content_as_string('cc_image_classifier.py'))
 
-    num_images_per_class = 500
-    page_size = min(500, num_images_per_class)
-    num_pages = ((num_images_per_class-1) // page_size) + 1
+    #classes, num_images_per_class = ui()
 
-    license = 'CC0'
-    params = {'q': None,
-              'license': license,
-              'page_size': page_size,
-             }
+    #num_images_per_class = 500
+    #page_size = min(500, num_images_per_class)
+    #num_pages = ((num_images_per_class-1) // page_size) + 1
 
-    queries = []
+    #license = 'CC0'
+    #params = {'q': None,
+    #          'license': license,
+    #          'page_size': page_size,
+    #         }
+
+    #queries = []
     #query_urls = get_urls(queries, num_pages, params)
+
+
+def instructions():
+    labels = {}
+    with open('input_labels.txt', 'r') as infile:
+        for i, line in enumerate(infile):
+            if i % 2 == 0:
+                key = line.strip('\n')
+            else:
+                labels[key] = line.strip('\n')
+
+
+    st.markdown("### Pick the kinds of images you want to classify")
+    classes = st.text_area(labels['classes'],
+                value="dog\ncat\ngothic architecture").split('\n')
+    st.write("Here's an example of valid input!")
+    st.markdown("![classes_input_example](https://" \
+                "raw.githubusercontent.com/warrenmo/" \
+                "cc-image-classifier/master/class_input_example.jpg)")
+
+    st.markdown('### Pick the number of photos per class')
+    num_photos_per_class = st.number_input(labels['num_photos_per_class'],
+                                           min_value=1,
+                                           max_value=1000,
+                                           value=200)
+    return classes, num_photos_per_class
+
+
+def download_images():
+    labels = {}
+    with open('input_labels.txt', 'r') as infile:
+        for i, line in enumerate(infile):
+            if i % 2 == 0:
+                key = line.strip('\n')
+            else:
+                labels[key] = line.strip('\n')
+
+
+    st.markdown("### What images do you want to classify?")
+    classes = st.text_area('').split('\n')
+
+    st.markdown('### How many photos per class?')
+    num_photos_per_class = st.number_input('',
+                                           min_value=1,
+                                           max_value=1000,
+                                           value=200)
+    return classes, num_photos_per_class
+
+
+def train_classifier():
+    pass
+
+
+def run_classifier():
+    pass
 
 
 def get_urls(queries, num_pages, params):
@@ -51,25 +124,6 @@ def _get_urls_one_q(q, params):
     return [res['url'] for res in r.json()['results']]
 
 
-def ui():
-    labels = {}
-    with open('input_labels.txt', 'r') as infile:
-        for i, line in enumerate(infile):
-            if i % 2 == 0:
-                key = line.strip('\n')
-            else:
-                labels[key] = line.strip('\n')
-
-
-    st.markdown('### Pick the kinds of images you want to classify')
-    classes = st.text_area(labels['classes']).split('\n')
-
-    st.markdown('### Pick the number of photos per class')
-    num_photos_per_class = st.number_input(labels['num_photos_per_class'],
-                                           min_value=1,
-                                           max_value=1000,
-                                           value=200)
-    return classes, num_photos_per_class
 
 
 if __name__ == "__main__":
