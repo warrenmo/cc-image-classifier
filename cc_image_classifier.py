efbbcd8e3958023a850775453fa62256b5923f03
warren <wmowkm@gmail.com>
2020-12-28 14:37:43 -0500

    verify image downloads, added warnings+errors

--- cc_image_classifier.py
+++ cc_image_classifier.py
@@ -8,7 +8,10 @@ import pandas as pd
 
 import streamlit as st
 
-from fastai.vision.utils import download_images
+from fastai.vision.utils import download_images, verify_images
+
+
+IMAGE_DIR = Path('./cic_images')
 
 
 def main():
@@ -71,16 +74,25 @@ def download_data():
 
     # TODO: handle empty lines
     classes = st.text_area(
-        "Specify the images you wish to classify here:",
+        "Specify the images you wish to classify:",
         value="dog\ncat\ngothic architecture"
         )
     classes = classes.split('\n')
+    if len(classes) < 2:
+        st.error("At least 2 classes must be given.")
     per_class = st.number_input(
-        "Specify the number of images per class here:",
+        "Specify the number of images per class:",
         min_value=1,
         max_value=1000,
         value=200
         )
+    # TODO: check to see if 50 images is enough
+    # TODO: # of images varies with # of classes
+    if per_class < 50:
+        st.warning((
+            "Warning: expect unreliable performance "
+            "with fewer than 50 images per class"
+            ))
     press = st.button("Submit & Download")
     if press:
         # TODO: improve human readability of output
@@ -96,6 +108,7 @@ def download_data():
 
         url_dict = _get_urls(classes, num_pages, page_size=page_size)
         _download_urls(url_dict)
+        _verify_downloads(url_dict)
 
 
 def _get_urls(queries, num_pages, *, page_size, **kwargs):
@@ -123,13 +136,45 @@ def _get_urls_one_q(q, *, page, page_size, license='CC0'):
     return [res['url'] for res in r.json()['results']]
 
 
-def _download_urls(url_dict, image_dir=Path('./cic_images')):
-    image_dir = Path('./cic_images')
+def _download_urls(url_dict, image_dir=IMAGE_DIR):
     image_dir.mkdir(exist_ok=True)
     for c, urls in url_dict.items():
         with st.spinner(f"Downloading images for: {c}"):
             download_images(image_dir/c.replace(' ', '_'), urls=urls)
         st.success(f"Success!")
+
+
+def _verify_downloads(url_dict, image_dir=IMAGE_DIR):
+    fails = _get_fails(url_dict, image_dir)
+    _delete_fails(fails)
+
+
+def _get_fails(url_dict, image_dir=IMAGE_DIR):
+    st.spinner("Verifying the images...")
+    if not image_dir.exists():
+        st.exception(
+            FileNotFoundError(f"No such directory: {image_dir.absolute()}")
+            )
+    fails = {
+        c: verify_images(Path.iterdir(image_dir/c)) for c in url_dict.keys()
+        }
+    return fails
+
+
+def _delete_fails(fails):
+    """
+    TODO: give users more info/control on which images to be deleted.
+    """
+    if sum(len(f) for f in fails.values()) == 0:
+        st.success("No images failed to open :sunglasses:")
+    else:
+        for c, f in fails.items():
+            if len(f) == 0:
+                continue
+            f.map(Path.unlink)
+            st.success(
+                f"Deleted {len(f)} images for class {c} that failed to open."
+                )
 
 
 def train_classifier():
@@ -152,6 +197,7 @@ def get_file_content_as_string(path):
         )
     response = urllib.request.urlopen(url)
     return response.read().decode('utf-8')
+
 
 if __name__ == "__main__":
     main()
