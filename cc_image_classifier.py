8e8ef133b659807dd5bf472034b9cd472993b8e8
warren <wmowkm@gmail.com>
2020-12-28 00:52:17 -0500

    download added, require fastai v2, input_labels now in .json

--- cc_image_classifier.py
+++ cc_image_classifier.py
@@ -1,3 +1,4 @@
+import json
 import requests
 from pathlib import Path
 
@@ -6,7 +7,7 @@ import pandas as pd
 
 import streamlit as st
 
-from fastai.vision import download_images
+from fastai.vision.utils import download_images
 
 
 def main():
@@ -21,76 +22,112 @@ def main():
         st.sidebar.success('To continue select "Download images".')
     elif app_mode == 'Download images':
         instructions.empty()
-        download_images()
+        download_data()
     elif app_mode == 'Train the classifier':
+        instructions.empty()
         train_classifier()
     elif app_mode == 'Use/download the classifier':
+        instructions.empty()
         run_classifier()
     elif app_mode == 'Show the source code':
         instructions.empty()
         st.code(get_file_content_as_string('cc_image_classifier.py'))
-
-    #classes, num_images_per_class = ui()
-
-    #num_images_per_class = 500
-    #page_size = min(500, num_images_per_class)
-    #num_pages = ((num_images_per_class-1) // page_size) + 1
-
-    #license = 'CC0'
-    #params = {'q': None,
-    #          'license': license,
-    #          'page_size': page_size,
-    #         }
 
     #queries = []
     #query_urls = get_urls(queries, num_pages, params)
 
 
 def instructions():
-    labels = {}
-    with open('input_labels.txt', 'r') as infile:
-        for i, line in enumerate(infile):
-            if i % 2 == 0:
-                key = line.strip('\n')
-            else:
-                labels[key] = line.strip('\n')
-
+    with open('input_labels.json', 'r') as j:
+        labels = json.load(j)
 
     st.markdown("### Pick the kinds of images you want to classify")
-    classes = st.text_area(labels['classes'],
-                value="dog\ncat\ngothic architecture").split('\n')
+    classes = st.text_area(
+        labels['classes'],
+        value="dog\ncat\ngothic architecture").split('\n')
     st.write("Here's an example of valid input!")
-    st.markdown("![classes_input_example](https://" \
-                "raw.githubusercontent.com/warrenmo/" \
-                "cc-image-classifier/master/class_input_example.jpg)")
+    st.markdown((
+        "![classes_input_example](https://"
+        "raw.githubusercontent.com/warrenmo/"
+        "cc-image-classifier/master/class_input_example.jpg)"
+        ))
 
     st.markdown('### Pick the number of photos per class')
-    num_photos_per_class = st.number_input(labels['num_photos_per_class'],
-                                           min_value=1,
-                                           max_value=1000,
-                                           value=200)
+    num_photos_per_class = st.number_input(
+        labels['num_photos_per_class'],
+        min_value=1,
+        max_value=1000,
+        value=200
+        )
     return classes, num_photos_per_class
 
 
-def download_images():
-    labels = {}
-    with open('input_labels.txt', 'r') as infile:
-        for i, line in enumerate(infile):
-            if i % 2 == 0:
-                key = line.strip('\n')
-            else:
-                labels[key] = line.strip('\n')
+def download_data():
+    # TODO: break up this overloaded function
+    classes, num_images_per_class = [], 1
+    st.markdown("# Download images")
+
+    # TODO: handle empty lines
+    classes = st.text_area(
+        "What images do you want to classify?",
+        value="dog\ncat\ngothic architecture"
+        )
+    classes = classes.split('\n')
+    per_class = st.number_input(
+        "How many images per class?",
+        min_value=1,
+        max_value=1000,
+        value=200
+        )
+    press = st.button("Submit & Download")
+    if press:
+        # TODO: improve human readability of output
+        st.text(f"{'Classes:':<28}{', '.join(classes):>40}")
+        st.text(f"{'Number of images per class:':<28}{per_class:>40}")
+        st.text((
+            f"{'Total number of images:':<28}"
+            f"{per_class*len(classes):>40}"
+            ))
+
+        page_size = min(500, per_class)
+        num_pages = ((num_images_per_class-1) // page_size) + 1
+
+        url_dict = _get_urls(classes, num_pages, page_size=page_size)
+        _download_urls(url_dict)
 
 
-    st.markdown("### What images do you want to classify?")
-    classes = st.text_area('').split('\n')
+def _get_urls(queries, num_pages, *, page_size, **kwargs):
+    query_urls = {}
+    for q in queries:
+        urls = []
+        for page in range(1, num_pages+1):
+            page_urls = _get_urls_one_q(
+                q, page=page, page_size=page_size, **kwargs
+                )
+            urls.extend(page_urls)
+        query_urls[q] = urls
+    return query_urls
 
-    st.markdown('### How many photos per class?')
-    num_photos_per_class = st.number_input('',
-                                           min_value=1,
-                                           max_value=1000,
-                                           value=200)
-    return classes, num_photos_per_class
+
+def _get_urls_one_q(q, *, page, page_size, license='CC0'):
+    params = {
+        'q': q,
+        'page': page,
+        'page_size': page_size,
+        'license': license
+        }
+    r = requests.get('https://api.creativecommons.engineering/v1/images',
+                     params=params)
+    return [res['url'] for res in r.json()['results']]
+
+
+def _download_urls(url_dict, image_dir=Path('./cic_images')):
+    image_dir = Path('./cic_images')
+    image_dir.mkdir(exist_ok=True)
+    for c, urls in url_dict.items():
+        with st.spinner(f"Downloading images for: {c}"):
+            download_images(image_dir/c.replace(' ', '_'), urls=urls)
+        st.success(f"Success!")
 
 
 def train_classifier():
@@ -99,31 +136,6 @@ def train_classifier():
 
 def run_classifier():
     pass
-
-
-def get_urls(queries, num_pages, params):
-    query_urls = {}
-    for q in queries:
-        params['q'] = q
-        urls = []
-
-        for page in range(1, num_pages+1):
-            params['page'] = page
-            page_urls = _get_urls_one_q(q, params)
-            urls.extend(page_urls)
-
-        query_urls[q] = urls
-    return query_urls
-
-
-def _get_urls_one_q(q, params):
-    params['q'] = q
-    r = requests.get('https://api.creativecommons.engineering/v1/images',
-                     params=params)
-    params['q'] = None
-    return [res['url'] for res in r.json()['results']]
-
-
 
 
 if __name__ == "__main__":
