164073687feea0dd1eee0353236158bd3f44c345
warren <wmowkm@gmail.com>
2020-12-28 23:56:20 -0500

    deleted unnecessary function & its contents

--- app.py
+++ app.py
@@ -31,32 +31,5 @@ def main():
         st.code(get_file_content_as_string('cc_image_classifier.py'))
 
 
-def instructions():
-    #with open('input_labels.json', 'r') as j:
-    #    labels = json.load(j)
-
-    #st.markdown("### Pick the kinds of images you want to classify")
-    #classes = st.text_area(
-    #    labels['classes'],
-    #    value="dog\ncat\ngothic architecture").split('\n')
-    #st.write("Here's an example of valid input!")
-    #st.markdown((
-    #    "![classes_input_example](https://"
-    #    "raw.githubusercontent.com/warrenmo/"
-    #    "cc-image-classifier/master/class_input_example.jpg)"
-    #    ))
-
-    #st.markdown('### Pick the number of photos per class')
-    #num_photos_per_class = st.number_input(
-    #    labels['num_photos_per_class'],
-    #    min_value=1,
-    #    max_value=1000,
-    #    value=200
-    #    )
-    #return classes, num_photos_per_class
-    #
-    pass
-
-
 if __name__ == "__main__":
     main()
